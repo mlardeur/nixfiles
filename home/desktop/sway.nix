a5139ffcea1cf59074eee48291fdd0e38d8cdf28
Maxime Lardeur <maxime@fruitionsciences.com>
2023-11-02 11:03:14 +0100

    Add sway declarative config

--- home/desktop/sway.nix
+++ home/desktop/sway.nix
@@ -7,6 +7,29 @@
 
   wayland.windowManager.sway = {
     enable = true;
+    systemd.enable = true;
+    systemd.xdgAutostart = false;
+    xwayland = true;
+    wrapperFeatures.gtk = true;
+    config = {
+      modifier = "Mod4";
+      terminal = "kitty";
+      menu = "rofi -show drun -show-icons";
+      gaps = {
+        inner = 8;
+        outer = 2;
+        smartGaps = true;
+      };
+      assigns = {
+        "2" = [{ app_id = "Chromium"; }];
+        "5" = [{ app_id = "firefox"; }];
+      };
+      bars = [{ command = "waybar"; }];
+      startup = [
+        { command = "autotiling"; always = true; }
+        { command = "nm-applet --indicator"; always = true; }
+      ];
+    };
   };
 
   home.file = {
--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -102,12 +102,12 @@
   services.getty.autologinUser = "maxime"; # Autologin user
 
   systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
         Type = "simple";
         ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
         Restart = "on-failure";
