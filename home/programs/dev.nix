8992768f0e48bcfe0f01498986fb1f35636bbc9e
Maxime <max.lardeur@gmail.com>
2023-12-10 17:31:59 +0100

    Only keep jetbrains toolbox

--- home/programs/dev.nix
+++ home/programs/dev.nix
@@ -4,9 +4,6 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     jetbrains-toolbox
-    jetbrains.datagrip
-    jetbrains.idea-community
-    jetbrains.webstorm
     vscode
   ];
 
