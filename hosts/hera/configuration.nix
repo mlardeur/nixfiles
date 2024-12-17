7dbd46878a184f9e11c3c04eae67a1d3dbbb3629
Maxime Lardeur <max.lardeur@gmail.com>
2024-12-17 21:47:13 +0100

    Fix temperature sensor in waybar

--- hosts/hera/configuration.nix
+++ hosts/hera/configuration.nix
@@ -45,6 +45,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable sound.
   services.pipewire = {
